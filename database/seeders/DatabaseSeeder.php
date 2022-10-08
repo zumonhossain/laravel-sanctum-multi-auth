b15b2bf5476cf72084b845a2a8a20aa5cd8399aa
zumonhossain <zumon15-13169@diu.edu.bd>
2022-10-08 20:18:36 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com',
+            'phone' => '01644954478',
+            'password' => Hash::make('11111111'),
+        ]);
+    }
+}
